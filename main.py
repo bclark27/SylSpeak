c23895bbee7e954813b22b94b40d14672856b468
Benjamin Clark <benjamin.surfer14@gmail.com>
2025-10-19 22:47:27 -0700

    script and words

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ learning_mode = "-l" in sys.argv or "--learn" in sys.argv
 en_mode = "-e" in sys.argv or "--english" in sys.argv
 space_mode = "-s" in sys.argv or "--space" in sys.argv
 
+chinese_mode = "-c" in sys.argv or "--chinese" in sys.argv
+tibet_mode = "-t" in sys.argv or "--tibet" in sys.argv
+
+mode_count = (1 if chinese_mode else 0) + (1 if tibet_mode else 0)
+if mode_count > 1:
+  print("more then one language mode active")
+  exit()
+
+if mode_count == 0:
+  chinese_mode = True
+
 DEFAULT_CUSTOM_YAML__NAME = "default.custom.yaml"
 LANG_SCHEMA_YAML__NAME = f"{lang_name}.schema.yaml"
 LANG_DICT_YAML__NAME = f"{lang_name}.dict.yaml"
@@ -54,7 +65,7 @@ engine:
     - script_translator
 
 speller:
-  alphabet: "abcdefghijklmnopqrstuvwxyz "
+  alphabet: "abcdefghijklmnopqrstuvwxyz"
   delimiter: " '"
 
 translator:
@@ -110,10 +121,158 @@ def clean_rime():
     if name.endswith(".userdb") and os.path.isdir(path):
         shutil.rmtree(path)
 
+# Base consonants for main/root letters
+base_consonants = {
+  'q': 'ྐ',
+  'w': 'ྑ',
+  'r': 'ྒ',
+  't': 'ྔ',
+  'y': 'ྕ',
+  'p': 'ྖ',
+  's': 'ྗ',
+  'd': 'ྙ',
+  'f': 'ྚ',
+  'g': 'ྛ',
+  'h': 'ྜ',
+  'j': 'ྞ',
+  'k': 'ྟ',
+  'l': 'ྰ',
+  'z': 'ླ',
+  'x': 'ྶ',
+  'c': 'ྴ',
+  'v': 'ྵ',
+  'b': 'ྷ',
+  'n': 'ྺ',
+  'm': 'ྻ',
+  '-': 'ྦ',
+  '0': 'ི',
+  '1': 'ུ',
+}
 
-def parse_vocab(fname):
+vowels = "aeiou"
+# Vowel marks (diacritics)
+vowel_marks = {
+
+    '_': 'ྼ',
+
+    'a': 'ཀ',
+    'e': 'ཁ',
+    'i': 'ག',
+    'o': 'གྷ',
+    'u': 'ང',
+
+    'aa': 'ཅ',
+    'ea': 'ཆ',
+    'ia': 'ཇ',
+    'oa': 'ཉ',
+    'ua': 'ཊ',
+
+    'ae': 'ཋ',
+    'ee': 'ཌ',
+    'ie': 'ཌྷ',
+    'oe': 'ཎ',
+    'ue': 'ཏ',
+    
+    'ai': 'ཐ',
+    'ei': 'ད',
+    'ii': 'དྷ',
+    'oi': 'ན',
+    'ui': 'པ',
+
+    'ao': 'ཕ',
+    'eo': 'ཬ',
+    'io': 'བྷ',
+    'oo': 'མ',
+    'uo': 'ཙ',
+
+    'au': 'ཚ',
+    'eu': 'ཛ',
+    'iu': 'ཛྷ',
+    'ou': 'ཛྷ',
+    'uu': 'ཟ',
+}
+
+def split_cv(word):
   
-  return vocab
+  groups = []
+  current = ""
+  current_type = None
+
+  for ch in word.lower():
+    if ch.isalpha():
+      if ch in vowels:
+        ch_type = "V"
+      else:
+        ch_type = "C"
+
+      if current_type == ch_type or current_type is None:
+        current += ch
+      else:
+        groups.append(current)
+        current = ch
+      current_type = ch_type
+  if current:
+    groups.append(current)
+  return groups
+
+def engToTbt(word):
+  word = word.strip()
+
+  groups = split_cv(word)
+  
+  consts1 = None
+  consts2 = None
+  vowelGroup = None
+
+  # find the first vowel group, as well as the const group before and after
+  for i in range(len(groups)):
+    g = groups[i]
+    isVowel = g[0] in vowels
+
+    if isVowel and vowelGroup is not None:
+      break
+
+    if isVowel:
+      vowelGroup = g
+      continue
+    
+    if vowelGroup is None:
+      consts1 = g
+      continue
+
+    if consts2 is None:
+      consts2 = g
+      continue
+    
+    break
+  
+  endChars = ''
+  if consts1 is not None:
+    endChars += '0'
+  if consts2 is not None:
+    endChars += '1'
+
+  midChar = ''
+  if consts1 is not None and consts2 is not None:
+    midChar = '-'
+
+  if consts1 is None:
+    consts1 = ''
+
+  if consts2 is None:
+    consts2 = ''
+
+  if vowelGroup is None:
+    vowelGroup = '_'
+
+  encoding = ''.join([consts1, midChar, consts2, endChars])
+  
+  baseChar = vowel_marks[vowelGroup]
+
+  for c in encoding:
+    baseChar += base_consonants[c]
+
+  return baseChar
 
 def generate_tab_seperated_vocab():
   vocab_lines = []
@@ -132,8 +291,12 @@ def generate_tab_seperated_vocab():
         continue
 
       raw_info = line.split(',')
-      raw_info[1] = expanded = re.sub(r'\[([^\]]+)\]', lambda m: char_dict.get(m.group(1), m.group(1)), raw_info[1])
-      char_dict[raw_info[0]] = raw_info[1]
+
+      if chinese_mode:
+        raw_info[1] = re.sub(r'\[([^\]]+)\]', lambda m: char_dict.get(m.group(1), m.group(1)), raw_info[1])
+        char_dict[raw_info[0]] = raw_info[1]
+      elif tibet_mode:
+        raw_info[1] = "".join([engToTbt(x) for x in raw_info[0].split(' ')]) + "།"
 
       if en_mode:
         raw_info[1] = f"{raw_info[0]} "
